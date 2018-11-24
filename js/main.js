5df9bf6cd1a51cc9bbfabeab0ae73662d3494d97
arpit <banatiarpit97@yahoo.co.in>
2018-11-25 05:11:13 +0530

    removed useless code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ angular.module('moleGame',['ngMaterial'])
     $('.minus1').hide();
 
     var game,
-    pause,
     score,
     lives,
     clicks = 0,
@@ -15,7 +14,6 @@ angular.module('moleGame',['ngMaterial'])
     liferemove;
 
     $("#mole").on('animationend webkitAnimationEnd oAnimationEnd', (e) => {
-        console.log(e.originalEvent.animationName)
         if((e.originalEvent.animationName.indexOf('hit') > -1) && (clicked)){
             createMole();                        
         }
@@ -29,7 +27,6 @@ angular.module('moleGame',['ngMaterial'])
                         noHitMole();
                     }   
                 },frequency);
-            // noHitMole();                        
         }
     })
 
@@ -105,31 +102,7 @@ angular.module('moleGame',['ngMaterial'])
         };
     };
 
-    $scope.pause = function(){
-        if($('.pausebtn').text() == 'Pause'||$('.pausebtn').text() == 'PAUSE'){
-            pause = true;
-            game = false;
-            $('.pausebtn').text('RESUME')
-            if($('#mole').css('animation') == 'moleexit 0.8s ease 0s 1 normal forwards running'){
-                // clearInterval(b);
-            }
-            $('#mole').css('animation-play-state', 'paused')
-
-        }
-        else{
-            pause = false;
-            game  = true;
-            // b = setInterval(nohitmole, frequency)
-            $('.pausebtn').text('PAUSE')
-            // if($('#mole').css('animation') == 'moleexit 0.8s ease 0s 1 normal forwards paused'){
-            //     $('#mole').one('animationend', function (e) {
-            //         createMole();
-            //     })
-            // }
-            $('#mole').css('animation-play-state', 'running')
-        }
-
-    }
+    
     $scope.restart = function(){
         $('.timer').html(3)
         $(".life").css('color','red');
@@ -139,7 +112,6 @@ angular.module('moleGame',['ngMaterial'])
         $scope.start();
     }
 
-	// $('#hammer').css(left, )
 	var holePositions = [{left:'13.5%', bottom:'20%', height:'115px', width:'106px'},
                          {left:'44.5%', bottom:'20%', height:'115px', width:'106px'},
                          {left:'72.5%', bottom:'20%', height:'115px', width:'106px'},
@@ -148,9 +120,6 @@ angular.module('moleGame',['ngMaterial'])
                          {left:'46.5%', bottom:'47%', height:'96px', width:'89px'},
                          {left:'10.5%', bottom:'43%', height:'96px', width:'89px'},
                          {left:'81.5%', bottom:'44%', height:'96px', width:'89px'}];
-    // createMole();
-    // clicked = false;
-	var bb = false;
 	var clicked;
 
 	function noHitMole(){
@@ -164,46 +133,11 @@ angular.module('moleGame',['ngMaterial'])
                 }, 1000)
             if(lives <= 0) {
                 game = false;
-                // clearInterval(b);
                 $('.gameover').removeClass('hidden').addClass('animated bounceInDown');
                 $('.whiteBack').css('opacity', '0.5');
                 $('.hole').css('opacity', '0.3')
                 $('.pausebtn').addClass('hidden')
             }
-
-
-	    // if(game && !clicked) {
-     //        bb = true;
-     //        lives--;
-     //        $('#mole').css({animation: 'moleexit 0.8s', 'animation-fill-mode': 'forwards'});
-     //        console.log($('#mole').css('animation'));
-     //        a = setTimeout(function () {
-     //          if(!clicked){
-
-     //            $('.minus1').fadeIn();
-     //            liferemove = setTimeout(function () {
-     //                $('.minus1').fadeOut();
-     //            }, 1000)
-     //            $('#hammer').css('animation', '');
-     //            l = lives + 1;
-     //            $(".life:nth-child(" + l + ")").css('color', 'grey')
-     //            if (lives > 0) {
-
-     //                createMole();
-     //            }
-     //            else {
-     //                clearInterval(b);
-     //                $('.gameover').removeClass('hidden').addClass('animated bounceInDown');
-     //                $('.whiteBack').css('opacity', '0.5');
-     //                $('.hole').css('opacity', '0.3')
-     //                $('.pausebtn').addClass('hidden')
-     //            }
-     //          }  
-     //          else{
-     //                createMole();
-     //          }
-     //        }, 1000)
-     //    }
     }
 
 	function createMole(){
@@ -216,7 +150,6 @@ angular.module('moleGame',['ngMaterial'])
                 toExit = null;
             }
             holeNumber = Math.floor(Math.random() * 8);
-            // holeNumber = 2;
             currentMolePosition = holePositions[holeNumber];
             $('#mole').css(currentMolePosition).removeClass('hidden');
             switch(holeNumber){
@@ -232,25 +165,14 @@ angular.module('moleGame',['ngMaterial'])
                 case 7: $('#mole').css({'animation': 'moleentry3 0.8s', 'animation-fill-mode': 'forwards'})
                         
             }
-
-            // if (!clicked && !bb) {
-            //     b = setInterval(nohitmole, frequency)
-            // }
         }
     }
     $('#mole').click(function(){
 
-    	if((!timeout) && (($('#mole').css('animation') != 'moleexit 1s ease 0s 1 normal forwards running') || ($('#mole').css('animation') != 'hit1 1s ease 0s 1 normal forwards running') || ($('#mole').css('animation') != 'hit2 1s ease 0s 1 normal forwards running') || ($('#mole').css('animation') != 'hit3 1s ease 0s 1 normal forwards running'|| (!clicked)))) {
+    	if((!timeout) && (!clicked)) {
             var animationNumber;
             clicked = true;
-            // clicks++;
             if(++clicks == 1){
-                // timer = setTimeout(function(){
-                // clicks = 0;
-                // }, clickDelay)
-
-                // clearInterval(b)
-                // bb = false;
                 score++;
                 $('.plus1').fadeIn();
                 scoreremove = setTimeout(function(){$('.plus1').fadeOut();}, 1000)
@@ -270,27 +192,10 @@ angular.module('moleGame',['ngMaterial'])
                     case 6:
                     case 7: $(this).css({'animation': 'hit3 1s', 'animation-fill-mode': 'forwards'})
                 }
-                // console.log('a', $('#hammer').css('animation'))
-                // console.log('a', $('#hammer').css('animation'))
                 $('#hammer').css({'animation': '' , 'animation-play-state':'initial'})
                 setTimeout(() => {
                         $('#hammer').css({'animation': 'hammer 0.4s', 'animation-iteration-count': 1,  'animation-fill-mode': 'forwards', 'animation-play-state':'initial'})
-
                     }, 0);
-
-                // $(this).on('animationend webkitAnimationEnd oAnimationEnd', (e) => {
-                //     console.log(e.originalEvent.animationName)
-                //     if((e.originalEvent.animationName.indexOf('hit') > -1) && (clicked)){
-                //         createMole();                        
-                //     }
-                // })
-
-                // $(this).css({animation: 'hit 1s', 'animation-fill-mode': 'forwards'});
-
-                // a = setTimeout(function () {
-                //     $('#hammer').css('animation', '');
-                //     createMole();
-                // }, 1000)
             }
 
 
@@ -316,14 +221,8 @@ angular.module('moleGame',['ngMaterial'])
         imgHeight = $('#hammer').height();
         mouseX = mouseX - (imgWidth / 2);
         mouseY = mouseY - (imgHeight / 2);
-        // if(!pause) {
-            //     $('#hammer').css({visibilty:visible})
-
-            $("#hammer").css({left: mouseX, top: mouseY});
-
-        // }
+        $("#hammer").css({left: mouseX, top: mouseY});
         });
-
 });
 
 
